ded57c9a52b90f13e5aa9051c3dc5b003af9f547
Siket Kristof <kristofsiket@Kristofs-MacBook.local>
2022-01-22 14:58:34 +0100

    added chakra ui, created initial router and component structure

--- chm-ui/chm-react/src/features/counter/Counter.tsx
+++ chm-ui/chm-react/src/features/counter/Counter.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 
+import {
+  Button, Input, Box, Text, Stack,
+} from '@chakra-ui/react';
 import { useAppSelector, useAppDispatch } from '../../app/hooks';
 import {
   decrement,
@@ -9,8 +12,6 @@ import {
   incrementIfOdd,
   selectCount,
 } from './counterSlice';
-import { Button, Input, Box, Text, Stack } from "@chakra-ui/react"
-
 
 export function Counter() {
   const count = useAppSelector(selectCount);
@@ -20,31 +21,31 @@ export function Counter() {
   const incrementValue = Number(incrementAmount) || 0;
 
   return (
-    <Box alignItems='center' justifyContent='center' align='center'>
-      <Stack alignItems='center' justifyContent='center' align='center' direction="row" spacing={1}>
-        <Button colorScheme='teal' onClick={() => dispatch(decrement())}>-</Button>
+    <Box alignItems="center" justifyContent="center" align="center">
+      <Stack alignItems="center" justifyContent="center" align="center" direction="row" spacing={1}>
+        <Button colorScheme="teal" onClick={() => dispatch(decrement())}>-</Button>
         <Text>{count}</Text>
-        <Button colorScheme='teal' onClick={() => dispatch(increment())}>+</Button>
+        <Button colorScheme="teal" onClick={() => dispatch(increment())}>+</Button>
       </Stack>
       <Box>
-      <Input
+        <Input
           margin={5}
           value={incrementAmount}
           size="lg"
-          placeholder='Amount'
+          placeholder="Amount"
           onChange={(e) => setIncrementAmount(e.target.value)}
         />
-      <Stack direction="row" spacing={2}>
-        <Button colorScheme='teal' variant='solid' onClick={() => dispatch(incrementByAmount(incrementValue))}>
-          Add Amount
-        </Button>
-        <Button colorScheme='teal' variant='outline' onClick={() => dispatch(incrementAsync(incrementValue))}>
-          Add Async
-        </Button>
-        <Button colorScheme='teal' variant='outline' onClick={() => dispatch(incrementIfOdd(incrementValue))}>
-          Add if odd
-        </Button>
-      </Stack>
+        <Stack direction="row" spacing={2}>
+          <Button colorScheme="teal" variant="solid" onClick={() => dispatch(incrementByAmount(incrementValue))}>
+            Add Amount
+          </Button>
+          <Button colorScheme="teal" variant="outline" onClick={() => dispatch(incrementAsync(incrementValue))}>
+            Add Async
+          </Button>
+          <Button colorScheme="teal" variant="outline" onClick={() => dispatch(incrementIfOdd(incrementValue))}>
+            Add if odd
+          </Button>
+        </Stack>
       </Box>
     </Box>
   );
